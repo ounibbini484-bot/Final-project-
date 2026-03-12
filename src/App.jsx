db2f43a41a096f513da1ca5605b8c53c9495fc3d
Oun Ibbini <ounibbini@outlook.com>
2026-03-12 09:02:47 +0400

    commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./pages/Home"
 import Navbar  from "./components/Navbar"
-
+import { Routes,Route } from 'react-router-dom'
+import Cart from "./pages/Cart"
 
 
 function App() {
@@ -9,7 +10,15 @@ function App() {
   return (
     <>
     <Navbar/>
-   <Home/> 
+    <Routes>
+      <Route path="/" element={ <Home/> }/>
+      <Route path="/cart" element={ <Cart/> }/>
+
+
+   
+
+    </Routes>
+  
     </>
   )
 }
