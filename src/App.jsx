922a51ff562dfb6b81184527bb2e8b64bf398c7b
Oun Ibbini <ounibbini@outlook.com>
2026-03-24 09:40:08 +0400

    chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes,Route } from 'react-router-dom'
 import Cart from "./pages/Cart"
 import CartItem from "./components/CartItem"
 import Checkout from "./pages/Checkout"
+import Chatbot from "./pages/Chatbot"
 
 
 function App() {
@@ -12,13 +13,16 @@ function App() {
   return (
     <>
     <Navbar/>
+   
     <Routes>
       <Route path="/" element={ <Home/> }/>
       <Route path="/cart" element={ <Cart/> }/>
       <Route path="/cartitem" element={ <CartItem/> }/>
       <Route path="/checkout" element={ <Checkout/> }/>
 
+
     </Routes>
+     <Chatbot/>
   
     </>
   )
