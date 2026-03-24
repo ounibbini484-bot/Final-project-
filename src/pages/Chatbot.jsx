3e9884932e0aeb0542d2dc6d237e06aef64fd883
Oun Ibbini <ounibbini@outlook.com>
2026-03-24 19:43:51 +0400

    commit message

--- src/pages/Chatbot.jsx
+++ src/pages/Chatbot.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Chatbot = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggleChat=()=> setIsOpen(!isOpen)
   return (
     <div className='chatbot-container'>
-      <button className='chatbot-toggle-btn'> <svg width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+      <button className='chatbot-toggle-btn'onClick={toggleChat}> {!isOpen ?(<svg width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
         <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-      </svg>
+      </svg>):(<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+<line x1="18" y1="6" x2="6" y2="18"></line>
+<line x1="6" y1="6" x2="18" y2="18"></line>
+</svg>)}
       </button>
     </div>
   )
