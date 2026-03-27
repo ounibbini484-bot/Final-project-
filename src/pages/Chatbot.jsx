957924176d70a263ed5c816b61596511edcf1133
Oun Ibbini <ounibbini@outlook.com>
2026-03-27 09:03:33 +0400

    commit message

--- src/pages/Chatbot.jsx
+++ src/pages/Chatbot.jsx
@@ -3,38 +3,33 @@ import React, { useRef, useState } from "react";
 const Chatbot = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [messages, setMessages] = useState([
-    {text: "Hi there! How can I help you today?", sender: 'bot'}
+    { text: "Hi there! How can I help you today?", sender: "bot" }
   ]);
   const messagesEndRef = useRef(null);
 
   const toggleChat = () => setIsOpen(!isOpen);
 
-  const handleQuickReply = (question)=>{
-    const userMsg = {text: question, sender: 'user'};
-    setMessages(prev => [...prev, userMsg]);
+  const handleQuickReply = (question) => {
+    const userMsg = { text: question, sender: "user" };
+    setMessages((prev) => [...prev, userMsg]);
 
-    //simulate bot thinking and replying ....
     setTimeout(() => {
       let botResponse = "I am not sure how to answer that yet!";
 
-      if(question === "Where is my order?"){
-        botResponse = "Your order #100064063 is currently out for delivery and should arrive within 20 minutes🛵";
-      }
-      
-      else if(question === "Cancel order"){
+      if (question === "Where is my order?") {
+        botResponse =
+          "Your order #100064063 is currently out for delivery and should arrive within 20 minutes🛵";
+      } else if (question === "Cancel order") {
         botResponse = "Cancelling your order, see you again 😊";
-      }
-
-      else if(question === "Where is the restaurant?"){
+      } else if (question === "Where is the restaurant?") {
         botResponse = "Inside Dubai Festival City - United Arab Emirates 🍝";
-      }
-      else if(question === "Talk to human"){
+      } else if (question === "Talk to human") {
         botResponse = "Connecting to you an agent... Please hold on 👱🏻";
       }
 
-      setMessages(prev => [...prev, {text:botResponse, sender: 'bot'}]);
+      setMessages((prev) => [...prev, { text: botResponse, sender: "bot" }]);
     }, 1000);
-  }
+  };
 
   return (
     <div className="chatbot-container">
@@ -69,31 +64,60 @@ const Chatbot = () => {
           </svg>
         )}
       </button>
+
       {isOpen && (
         <div className="chatbot-window">
           <div className="chatbot-header">
-            <span>Support Chat </span>
+            <span>Support Chat</span>
             <button className="chatbot-close-btn" onClick={toggleChat}>
               &times;
             </button>
           </div>
+
           <div className="chatbot-messges">
-           {messages.map((msg, idx) =>(
-            <div key={idx} className={`chat-message ${msg.sender}`}>
-              {msg.text}
-            </div>
-           ))}
-           <div ref={messagesEndRef}/>
+            {messages.map((msg, idx) => (
+              <div key={idx} className={`chat-message ${msg.sender}`}>
+                {msg.sender === "bot" && (
+                  <img
+                    src="https://img.freepik.com/free-vector/graident-ai-robot-vectorart_78370-4114.jpg"
+                    alt="bot"
+                    className="chat-avatar"
+                  />
+                )}
+                {msg.text}
+              </div>
+            ))}
+            <div ref={messagesEndRef} />
           </div>
 
           <div className="chatbot-quick-replies">
-            <button className="quick-reply-btn" onClick={() => handleQuickReply("Where is my order?")}>Where is my order?</button>
-            
-            <button className="quick-reply-btn" onClick={() => handleQuickReply("Cancel order")}>Cancel order</button>
-            
-            <button className="quick-reply-btn" onClick={() => handleQuickReply("Where is the restaurant?")}>Where is the restaurant?</button>
-            <button className="quick-reply-btn" onClick={() => handleQuickReply("Talk to human")}>Talk to human</button>
-            
+            <button
+              className="quick-reply-btn"
+              onClick={() => handleQuickReply("Where is my order?")}
+            >
+              Where is my order?
+            </button>
+
+            <button
+              className="quick-reply-btn"
+              onClick={() => handleQuickReply("Cancel order")}
+            >
+              Cancel order
+            </button>
+
+            <button
+              className="quick-reply-btn"
+              onClick={() => handleQuickReply("Where is the restaurant?")}
+            >
+              Where is the restaurant?
+            </button>
+
+            <button
+              className="quick-reply-btn"
+              onClick={() => handleQuickReply("Talk to human")}
+            >
+              Talk to human
+            </button>
           </div>
         </div>
       )}
