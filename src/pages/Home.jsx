b5c0fc1898d2e36ff815832a42210829d0d314dd
Oun Ibbini <ounibbini@outlook.com>
2026-03-30 07:59:35 +0400

    commit message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import FoodCard from '../components/FoodCard'
 import { foodData } from '../data/foods'
 
 const Home = () => {
   return (
     <div className="container">
-      <h1 className="page-title">Explore Our Menu</h1>
+      
+      <div className="home-header">
+        <h1 className="page-title">Explore Our Menu</h1>
+
+        <div className="auth-actions">
+          <Link to="/login" className="login-btn">Login</Link>
+          <Link to="/signup" className="signup-btn">Sign Up</Link>
+        </div>
+      </div>
 
       <div className="food-container">
         {foodData.map((food) => (
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>Login
+       <p> Do not have a acont  ? Create one now  <Link to="/signup">Signup </Link></p>
+    </div>
   )
 }
 
 export default Login
+
+// <p>Already have an account ? <Link to="/login">Login</Link></p>
