f147870784e855f40427e8af260ab1dfee6207c9
Jawwad Umar <jawwadumar@Jawwads-MacBook-Pro.local>
2026-03-31 12:01:16 +0530

    navigate added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Login = () => {
 
@@ -7,6 +7,8 @@ const Login = () => {
       email:'',
       password:'',
     })
+
+      const navigate = useNavigate();
 
     const handleChange = (e)=>{
     setFormData({...formData, [e.target.name]: e.target.value})
