b67716c02e38029cbd8c264ffd028536e7308851
Jawwad Umar <jawwadumar99@gmail.com>
2026-05-01 12:02:50 +0530

    fetching food data from backned

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import FoodCard from '../components/FoodCard'
-import { foodData } from '../data/foods'
+import { useState } from 'react'
+import axios from 'axios'
 
 const Home = () => {
+  const [foodData, setFoodData] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchFood = async () => {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        navigate('/login');
+        return;
+      }
+
+      try {
+        const response = await axios.get('http://localhost:8081/food/', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = response.data;
+        setFoodData(Array.isArray(data) ? data : (data.data || []));
+      }
+      catch (err) {
+        console.error("Error:", err);
+        if (err.response && err.response.status == 401 || err.response.status) {
+          localStorage.removeItem('token');
+          navigate('/login');
+
+        }
+        alert("An error occurred during login");
+      }
+    };
+    fetchFood();
+  }, [navigate]);
+
   return (
     <div className="container">
 
-        <h1 className="page-title">Explore Our Menu</h1>
-     
+      <h1 className="page-title">Explore Our Menu</h1>
+
 
       <div className="food-container">
         {foodData.map((food) => (
-          <FoodCard key={food.id} food={food} />
+          <FoodCard key={food.id || food._id} food={food} />
         ))}
       </div>
 
